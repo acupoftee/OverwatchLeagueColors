2c19405cb358cb12ecb2e2804597d0d9b66e43a8
acupoftee <bdiakaba@uccs.edu>
2019-02-28 15:33:02 -0700

    primary color test passed

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 import {
-  mainSchema,
+  mainSchema, colorSchema,
 } from './schema';
 import {
-  getAllColors,
+  getAllColors, getPrimaryColor, getSecondaryColor,
 } from '..';
 
 describe('OwlColor', () => {
@@ -17,6 +17,33 @@ describe('OwlColor', () => {
       const validateResult = Joi.validate(allColors, mainSchema);
 
       expect(validateResult.error).toBe(null);
+    });
+  });
+
+  describe('getPrimaryColor', () => {
+    it('should exist', () => {
+      expect(getPrimaryColor).toBeDefined();
+    });
+
+    it('should return undefined if pass excluded team abbreviation', () => {
+      const teamPrimaryColor = getPrimaryColor('BB');
+      expect(teamPrimaryColor).toBe(undefined);
+    });
+
+    it('should match colorSchema is pass includes uppercase abbreviation', () => {
+      const teamPrimaryColor = getPrimaryColor('BOS');
+      const validateResult = Joi.validate(teamPrimaryColor, colorSchema);
+      expect(validateResult.error).toBe(null);
+      expect(teamPrimaryColor).toEqual({
+        hex: '#174B97',
+        rgb: [23, 75, 151],
+      });
+    });
+  });
+
+  describe('getSecondaryColor', () => {
+    it('should exist', () => {
+      expect(getSecondaryColor).toBeDefined();
     });
   });
 });
