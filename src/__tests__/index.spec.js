32728c2aa0ff80fee3094c766274f5e6afa8fae0
acupoftee <bdiakaba@uccs.edu>
2019-02-28 16:14:53 -0700

    get colors object test passed

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 import {
-  mainSchema, colorSchema,
+  mainSchema, colorSchema, colorsSchema,
 } from './schema';
 import {
-  getAllColors, getPrimaryColor, getSecondaryColor, getTertiaryColor,
+  getAllColors, getPrimaryColor, getSecondaryColor, getTertiaryColor, getColors,
 } from '..';
 
 describe('OwlColor', () => {
@@ -39,6 +39,11 @@ describe('OwlColor', () => {
         rgb: [23, 75, 151],
       });
     });
+
+    it('should have a defined hex string if given a property value', () => {
+      const hexColor = getPrimaryColor('BOS').hex;
+      expect(hexColor).toEqual('#174B97');
+    });
   });
 
   describe('getSecondaryColor', () => {
@@ -60,6 +65,11 @@ describe('OwlColor', () => {
         rgb: [239, 223, 0],
       });
     });
+
+    it('should have a defined hex string if given a property value', () => {
+      const hexColor = getSecondaryColor('BOS').hex;
+      expect(hexColor).toEqual('#EFDF00');
+    });
   });
 
   describe('getTertiaryColor', () => {
@@ -72,13 +82,69 @@ describe('OwlColor', () => {
       expect(teamTertiaryColor).toBe(undefined);
     });
 
-    it('should match colorSchea if pass includs uppercase abbreviation', () => {
+    it('should match colorSchea if pass includes uppercase abbreviation', () => {
       const teamTertiaryColor = getTertiaryColor('BOS');
       const validateResult = Joi.validate(teamTertiaryColor, colorSchema);
       expect(validateResult.error).toBe(null);
       expect(teamTertiaryColor).toEqual({
         hex: '#000000',
         rgb: [0, 0, 0],
+      });
+    });
+
+    it('should have a defined hex string if given a property value', () => {
+      const hexColor = getTertiaryColor('BOS').hex;
+      expect(hexColor).toEqual('#000000');
+    });
+  });
+
+  describe('getColors', () => {
+    it('should exist', () => {
+      expect(getColors).toBeDefined();
+    });
+
+    it('should return undefined if passed an improper abbreviation', () => {
+      const teamColors = getColors('OWO');
+      expect(teamColors).toBe(undefined);
+    });
+
+    it('should match colorSchea if pass includes uppercase abbreviation', () => {
+      const teamColors = getColors('DAL');
+      const validateResult = Joi.validate(teamColors, colorsSchema);
+      expect(validateResult.error).toBe(null);
+      expect(teamColors).toEqual({
+        darkBlue: {
+          hex: '#032340',
+          rgb: [3, 35, 64],
+        },
+        strongBlue: {
+          hex: '#0072ce',
+          rgb: [0, 114, 206],
+        },
+        gray: {
+          hex: '#9EA2A2',
+          rgb: [158, 162, 162],
+        },
+      });
+    });
+
+    it('should match colorSchea if pass includes lowercase abbreviation', () => {
+      const teamColors = getColors('dal');
+      const validateResult = Joi.validate(teamColors, colorsSchema);
+      expect(validateResult.error).toBe(null);
+      expect(teamColors).toEqual({
+        darkBlue: {
+          hex: '#032340',
+          rgb: [3, 35, 64],
+        },
+        strongBlue: {
+          hex: '#0072ce',
+          rgb: [0, 114, 206],
+        },
+        gray: {
+          hex: '#9EA2A2',
+          rgb: [158, 162, 162],
+        },
       });
     });
   });
